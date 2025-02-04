4697fc40f40b658f5c963ab00dc419bb3792b201
sserranom <sergiomelgara@gmail.com>
2025-02-04 17:23:47 +0100

    dejamos la clase Main unicamente para a interfaz con el usuario Merge branch 'master' of https://github.com/sserranom/S102-Exceptions-Nivel2.git

--- src/EjercicioN2/Entrada.java
+++ src/EjercicioN2/Entrada.java
@@ -12,11 +12,8 @@ public class Entrada {
 			
 			byte dato = 0;
 			boolean isByte = false;
-			
-			do {
-				
+			do{
 				try {
-					
 					System.out.println(mensaje);
 					dato = input.nextByte();
 					//input.nextLine();
@@ -24,283 +21,181 @@ public class Entrada {
 				
 				}catch (InputMismatchException e){
 					System.out.println("El dato introducido no es de tipo Byte, Ingresa el tipo de dato solicitado");
-					input.nextLine();
-						
+					input.nextLine();	
 				}
 			
 		     }while(!isByte);
 			
 			System.out.print("Byte " + dato);
-			return dato;
-			
-			
-			
+			return dato;	
         }
-		
-		
-		
+
 		public static int leerInt(String mensaje) {
 			
 			int dato=0;
 			boolean isInt = false;
 			
 			do {
-				
 				try {
-					
 					System.out.println(mensaje);
 					dato = input.nextInt();
 					//input.nextLine();
 					isInt = true;
-				
 				}catch (InputMismatchException e){
 					System.out.println("El dato Introducido no es de tipo Entero, Ingresa el tipo de dato solicitado");
 					input.nextLine();
-						
 				}
-			
 		     }while(!isInt);
-			
-			
 			System.out.print("Int " + dato);
 			return dato;
-			
-			
         }
-		
 		
 		
 		public static float leerFloat(String mensaje) {
 			
 			float dato = 0;
 			boolean isfloat = false;
-			
-			do {
-				
-				try {
-					
+			do{
+				try{
 					System.out.println(mensaje);					
 					String numero = input.next();
 					
 					if(numero.contains(".")) {
-						
-						 System.out.println("Error de Fomato, ingresa el decimal con una coma");
-						 
+						 System.out.println("Error de Fomato, ingresa el decimal con una coma");	 
 					}else if(numero.contains(",") && numero.endsWith("f")) {
-						
 						numero = numero.replace(",", ".");
 						
 						try {
-							
 							dato = Float.parseFloat(numero);
 							isfloat = true;
-							
 						}catch(NumberFormatException e){
-							
-							System.out.println("Error de formato p");
-							
+							System.out.println("Error de formato p");	
 						}
-						
 					}else {
-						
 						try {
-							
 							dato = Integer.parseInt(numero);
 							System.out.println("El dato no es válido, ingresa un numero con decimales");
-							
 						}catch (NumberFormatException e) {
-							
 							System.out.println("El dato Introducido no es de tipo Float, Ingresa el tipo de dato solicitado ej: 2.33f");
 						}
-						
 					}
 					
 							
 				}catch (InputMismatchException e){
-					
 					System.out.println("El dato Introducido no es de tipo Float, Ingresa el tipo de dato solicitado");
 					input.nextLine();
-						
 				}
 			
 		     }while(!isfloat);
 			
-			
 			System.out.print("Float " + dato);
 			return dato;
-			
         }
-		
-		
 		
 		public static double leerDouble(String mensaje) {
 			
 			double dato = 0;
 			boolean isdouble = false;
-			
+	
 			do {
-				
-				try {
-					
+				try {	
 					System.out.println(mensaje);					
 					String numero = input.next();
 					
-					if(numero.contains(".")) {
-						
+					if(numero.contains(".")) {	
 						 System.out.println("Error de Fomato, ingresa el decimal con una coma");
-						 
 					}else if(numero.contains(",") && !(numero.contains("f"))){
-						
 						numero = numero.replace(",", ".");
-						
 						try {
-							
 							dato = Double.parseDouble(numero);
 							isdouble = true;
-							
 						}catch(NumberFormatException e){
-							
 							System.out.println("Error de formato p");
-							
 						}
-						
 					}else {
-						
-						try {
-							
+						try{
 							dato = Integer.parseInt(numero);
 							System.out.println("El dato no es válido, ingresa un numero con decimales");
-							
 						}catch (NumberFormatException e) {
-							
 							System.out.println("El dato Introducido no es de tipo Double, Ingresa el tipo de dato solicitado ej: 2,3333333");
 						}
-						
-					}
-					
-							
+					}			
 				}catch (InputMismatchException e){
-					
 					System.out.println("El dato Introducido no es de tipo Double, Ingresa el tipo de dato solicitado");
 					input.nextLine();
-						
 				}
 			
 		     }while(!isdouble);
 			
-			
 			System.out.print("Double " + dato);
 			return dato;
-			
         }
 		
-		
-		
 		public static char leerChar(String mensaje) throws InvalidDataException {
-			
-		
-				
+					
 				System.out.println(mensaje);
 				String dato = input.nextLine();
 				char caracter;
 					
-				if(dato.length() != 1) {
-					
+				if(dato.length() != 1) {		
 				throw new InvalidDataException("Debes ingresar un unico Caracter para que sea válido");
-					
 				}
 					
 				caracter = dato.charAt(0);	
 				System.out.print("Char " + caracter);	
-				return caracter;	
-				
+				return caracter;			
 		}
 		
-		
-		
 		public static String leerString(String mensaje) throws InvalidDataException{
-			
 			
 				System.out.println(mensaje);
 				String dato = input.nextLine();
 				Float isfloat;
 				int isInt;
 				//boolean isString = false;
-				
-				
-				
-				
-					
-					if (dato.length()<=1){
-						
-						throw new InvalidDataException("Debes Ingresar una cadena de carateres");
-						
+			
+				if (dato.length()<=1){
+					throw new InvalidDataException("Debes Ingresar una cadena de carateres");
+				}
+				if(dato.contains(".") || dato.contains(",")){
+					try {
+						isfloat = Float.parseFloat(dato);
+						throw new InvalidDataException("Debes Ingresar una cadena de carateres");	
+					}catch(NumberFormatException e) {
+						System.out.println(dato + " Es un String");
 					}
-					
-					if(dato.contains(".") || dato.contains(",")){
-						
-						try {
-							
-							isfloat = Float.parseFloat(dato);
-							throw new InvalidDataException("Debes Ingresar una cadena de carateres");
-							
-						}catch(NumberFormatException e) {
-							
-							System.out.println(dato + " Es un String");
-							
-						}
-					}else {
-						
-						try {
-							
+					}else{
+						try{
 							isInt = Integer.parseInt(dato);
 							throw new InvalidDataException("Debes Ingresar una cadena de carateres");
-							
 						}catch(NumberFormatException e) {
-							
 							//System.out.println(dato + " Es un String");
-							
 						}
-						
 					}
 					
 					System.out.print("String " + dato);
 					return dato;
 		}
- 
-		
+ 		
 		public static boolean leerSINO(String mensaje) throws InvalidDataException{
 			
 				boolean dato = false;
 				//char caracter;
-				
 				System.out.println(mensaje);
 				String opcion = input.nextLine();
 				
 				if(opcion.length() > 1) {
-					
 					throw new InvalidDataException("Debes elegir la opcion correcta S/N");
-					
 				}else if(opcion.contains("s") || opcion.contains("S")) {
-					
 					dato = true;
-					
 				}else if(opcion.contains("n") || opcion.contains("N")) {
-					
 					dato = false;
-				
-				}else {
-					
+				}else{
 					throw new InvalidDataException("Debes elegir la opcion correcta S/N");
-					
 				}
 			
 				System.out.print(dato);
 				return dato;
-		
-			
 		}
 		
-		
-
 } 
--- src/EjercicioN2/Main.java
+++ src/EjercicioN2/Main.java
@@ -2,14 +2,13 @@ package EjercicioN2;
 
 import java.util.Scanner;
 
-public class LeerEntrada {
+public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.println("******Elija la opcion que desea Comprobar******\n");
 		System.out.println(" 1. Leer Byte \n 2. Leer Int \n 3. Leer Float \n 4. Leer Double"
 				+ "\n 5. Leer Char \n 6. Leer String \n 7. Leer SINO ");
-		
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -18,87 +17,56 @@ public class LeerEntrada {
 		switch(opcion) {
 			
 			case 1:
-				
 				Entrada.leerByte("Ingresa un dato de tipo byte: ");
 			break;
 			
-			
-			case 2:
-				
+			case 2:	
 				Entrada.leerInt("Ingrese un dato de tipo Int: ");
 			break;
 			
-			
 			case 3:
-			
 				Entrada.leerFloat("Ingresa un dato de tipo float: ");
 			break;
 			
-			
-			case 4:
-				
+			case 4:	
 				Entrada.leerDouble("Ingresa un dato de tipo Double: ");
 			break;
 			
-			
-			case 5:
-				
-				do {
-				
-					try {
-						
+			case 5:	
+				do{
+					try{	
 						Entrada.leerChar("Ingrese un dato de un solo caracter: ");
 						break;
-						
 					}catch(InvalidDataException e){
-						
 						System.out.println(e.getMessage());
-						
 					}	
-				
 				}while(true);
 			break;
 			
-			
-			case 6:
-				
+			case 6:	
 				boolean isString = false;
-				do {
-					try {
-						
+				do{
+					try{
 						Entrada.leerString("Ingresa un dato de tipo String: ");
 						isString = true;
-						
 					}catch(InvalidDataException e) {
-						
 						System.out.println(e.getMessage());
 					}
-					
 				}while(!isString);
 			break;
 			
-			
 			case 7:
-				
 				boolean isTrue = false;
-				do {
-					
-					try {
-						
+				do{
+					try{
 						Entrada.leerSINO("Digite S Rara devolver True o  N para devolver False (S/N)");
 						isTrue = true;
-						
 					}catch(InvalidDataException e) {
-						
 						System.out.println(e.getMessage());
-						
 					}
-						
 				}while(!isTrue);
 			break;	
 				
-		}
-			
+		}		
 	}
-	
 }
